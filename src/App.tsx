18bbc5e7cf87c34d3be01ae93b1e6472d4fa1337
diptigandhi24 <gandhidipti.s@gmail.com>
2020-09-27 09:30:33 +0530

    Adding the new routes for the player registration

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import Home from './Home';
 import Board from './board';
 import AddSecondPlayer from './addSecondPlayer';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import RegisterPlayer1 from './Components/RegisterPlayers/registerPlayer1';
+import RegisterPlayer2 from './Components/RegisterPlayers/registerPlayer2';
 let count = 0;
 function App() {
     return (
         <Router>
             <div className="App">
                 <Switch>
-                    <Route path="/register-player/:player-registration?/:gameId?" component={RegisterPlayer} />
+                    {/* <Route path="/register-player/:playerRegistration?/:gameId?" component={RegisterPlayer} /> */}
+                    <Route path="/register-player1/:playerRegistration?/:gameId?" component={RegisterPlayer1} />
+                    <Route path="/register-player2/:playerRegistration?/:gameId?" component={RegisterPlayer2} />
                     <Route path="/" exact component={Home} />
                     <Route path="/add-second-player" component={AddSecondPlayer} />
-                    <Route path="/board/:playerName" component={() => <Board key={(count += 1)} />} />
+                    <Route path="/board/" component={() => <Board key={(count += 1)} />} />
                 </Switch>
             </div>
         </Router>
