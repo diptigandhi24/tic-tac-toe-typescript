380dfb8120ebe813bf06f50cb1674690b300e94c
diptigandhi24 <gandhidipti.s@gmail.com>
2020-10-13 16:36:15 +0530

    testing the UL coponent, this file will be delted in future

--- src/stories/1-Button.stories.tsx
+++ src/stories/1-Button.stories.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from './Button';
 import Board from './boardtest';
+import createBoard from '../Components/BoardUI/CreateBoard';
 
+export const ButtonStory = () => {
+    const [player, nextPlayer] = useState<Array<string>>(Array(16).fill(''));
+    const handleClick = (event: React.MouseEvent<HTMLElement>): void => {
+        console.log('ID', event.currentTarget.id);
+        nextPlayer(prevState => {
+            const updateBoard: Array<string> = [...prevState];
+            updateBoard[parseInt(event.currentTarget.id)] = 'x';
+            return updateBoard;
+        });
+    };
+    return (
+        <ul>
+            <li>Hello</li>
+        </ul>
+    );
+};
 export default {
     title: 'Button',
     component: Button,
