7529f5a62c3ac10798d3cc17112915fcb7fc02f2
diptigandhi24 <gandhidipti.s@gmail.com>
2020-10-13 16:33:53 +0530

    refactor the imported components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import { RegisterPlayer } from './registerPlayer';
 import Home from './Home';
-import Board from './board';
-import AddSecondPlayer from './addSecondPlayer';
+import Game from './Components/Game/Game';
+import AddSecondPlayer from './Components/RegisterPlayers/AddSecondPlayer';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import RegisterPlayer1 from './Components/RegisterPlayers/registerPlayer1';
 import RegisterPlayer2 from './Components/RegisterPlayers/registerPlayer2';
-let count = 0;
-function App() {
+
+function App(): JSX.Element {
     return (
         <Router>
             <div className="App">
@@ -18,7 +17,7 @@ function App() {
                     <Route path="/register-player2/:playerRegistration?/:gameId?" component={RegisterPlayer2} />
                     <Route path="/" exact component={Home} />
                     <Route path="/add-second-player" component={AddSecondPlayer} />
-                    <Route path="/board/" component={() => <Board key={(count += 1)} />} />
+                    <Route path="/board/" component={Game} />
                 </Switch>
             </div>
         </Router>
